b86b66f54f06dac5c20eeb6544a947dbad9a1d78
Joseph Lenox <lenox.joseph@gmail.com>
2018-02-08 14:57:12 -0800

    Updating libslic3r documentation from slic3r repository. Commit: https://github.com/alexrj/Slic3r/commit/05461b862090239040cff875a2f43410e880711f.

--- libslic3r-doc/search/files_2.js
+++ libslic3r-doc/search/files_2.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['clipperutils_2ecpp',['ClipperUtils.cpp',['../_clipper_utils_8cpp.html',1,'']]],
   ['clipperutils_2ehpp',['ClipperUtils.hpp',['../_clipper_utils_8hpp.html',1,'']]],
-  ['conditionalgcode_2ecpp',['ConditionalGcode.cpp',['../_conditional_gcode_8cpp.html',1,'']]],
-  ['conditionalgcode_2ehpp',['ConditionalGcode.hpp',['../_conditional_gcode_8hpp.html',1,'']]],
+  ['conditionalgcode_2ecpp',['ConditionalGCode.cpp',['../_conditional_g_code_8cpp.html',1,'']]],
+  ['conditionalgcode_2ehpp',['ConditionalGCode.hpp',['../_conditional_g_code_8hpp.html',1,'']]],
   ['config_2ecpp',['Config.cpp',['../_config_8cpp.html',1,'']]],
   ['config_2ehpp',['Config.hpp',['../_config_8hpp.html',1,'']]],
   ['coolingbuffer_2ecpp',['CoolingBuffer.cpp',['../_cooling_buffer_8cpp.html',1,'']]],
